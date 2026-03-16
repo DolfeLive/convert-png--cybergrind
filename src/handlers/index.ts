9e33adadc27b43c7579067c17c57733c836ce114
DolfeLive <github@dolfelive.org>
2026-03-16 19:10:13 +1100

    Update index

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -68,6 +68,7 @@ import xcfHandler from "./xcf.ts";
 import pdfparseHandler from "./pdfparse.ts";
 import { tarGzHandler, tarZstdHandler, tarXzHandler } from "./tarCompressed.ts";
 import mclangHandler from "./minecraftLangfileHandler.ts";
+import cybergrindHandler from "./cybergrindHandler.ts";
 
 const handlers: FormatHandler[] = [];
 try { handlers.push(new svgTraceHandler()) } catch (_) { };
@@ -144,5 +145,6 @@ try { handlers.push(tarGzHandler) } catch (_) { };
 try { handlers.push(tarZstdHandler) } catch (_) { };
 try { handlers.push(tarXzHandler) } catch (_) { };
 try { handlers.push(new mclangHandler()) } catch (_) { };
+try { handlers.push(new cybergrindHandler()) } catch (_) { };
 
 export default handlers;
